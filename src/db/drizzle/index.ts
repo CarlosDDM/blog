0f29d93ca25bddb5764759aa1314ba50d71fd5e6
Carlos Daniel <carlosdanieldiasmendes@gmail.com>
2026-02-26 00:36:48 -0300

    ajustes

--- src/db/drizzle/index.ts
+++ src/db/drizzle/index.ts
@@ -2,8 +2,12 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import { postsTable, usersTable } from './schemas';
 import Database from 'better-sqlite3';
 import { resolve } from 'path';
+import { mkdirSync } from 'fs';
 
-const sqliteDatabasePath = resolve(process.cwd(), 'db.sqlite3');
+const dataDir = resolve(process.cwd(), 'data');
+mkdirSync(dataDir, { recursive: true });
+
+const sqliteDatabasePath = resolve(dataDir, 'db.sqlite3');
 const sqliteDatabase = new Database(sqliteDatabasePath);
 
 export const drizzleDb = drizzle(sqliteDatabase, {
